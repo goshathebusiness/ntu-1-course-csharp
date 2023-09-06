ebc69f45745e86dfea5cc8b53ad822f33ee7a64c
Illia Yalovoi <imperio091@gmail.com>
2023-09-06 12:31:59 +0300

    l1

---
+++ l1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace l1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter X");
+            string value = Console.ReadLine();
+            double x;
+            try
+            {
+                x = double.Parse(value);
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Error: not numeral elements in X");
+                return;
+            }
+
+
+            double result = Math.Pow(x, x + 3) + Math.Tan(x);
+
+            Console.WriteLine(result);
+        }
+    }
+}
