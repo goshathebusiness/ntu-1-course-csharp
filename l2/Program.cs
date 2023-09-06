264a366d381f59ede8652f99563905a938620370
Illia Yalovoi <imperio091@gmail.com>
2023-09-06 13:21:29 +0300

    l2

---
+++ l2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace l1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle tri1 = new Triangle();
+
+            tri1.SetASide(3);
+            tri1.SetBSide(4);
+            tri1.SetASide(8);
+
+            Console.WriteLine(tri1.GetASide());
+
+            Console.WriteLine(tri1.GetSquare());
+
+            tri1.SetASide(35);
+
+            Console.WriteLine(tri1.GetSquare());
+        }
+    }
+}
---
+++ l2/Triangle.cs
@@ -0,0 +1,32 @@
+namespace l1;
+
+public class Triangle
+{
+    private
+        double a, b, c;
+
+    public Triangle()
+    {
+        Console.WriteLine($"Triangle was successfully created");
+    }
+    ~Triangle()
+    {
+        Console.WriteLine($"Triangle was successfully destroyed");
+    }
+
+    public void SetASide(double value) {a = value;}
+    public double GetASide() {return a;}
+
+    public void SetBSide(double value) {b = value;}
+    public double GetBSide() {return b;}
+
+    public void SetCSide(double value) {c = value;}
+    public double GetCSide() {return c;}
+
+    public double GetSquare()
+    {
+        double perimeter = a + b + c;
+        return Math.Sqrt(perimeter * (perimeter - a) * (perimeter - b) * (perimeter - c));
+    }
+
+}
